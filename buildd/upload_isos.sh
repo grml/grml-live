a090ad82e812624a5e67b957f5b50878b28b457e
Michael Prokop <mika@grml.org>
2007-10-18 00:33:40 +0200

    Update of buildd scripts

--- buildd/upload_isos.sh
+++ buildd/upload_isos.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
-# Filename:      upload_isos.sh
-# Purpose:       upload generated ISOs to daily.grml.org
-# Authors:       grml-team (grml.org), (c) Michael Prokop <mika@grml.org>
-# Bug-Reports:   see http://grml.org/bugs/
-# License:       This file is licensed under the GPL v2 or any later version.
-# Latest change: Mit Okt 10 09:46:08 CEST 2007 [mika]
-################################################################################
-
-cd /grml-live/grml-isos || exit 1
+. main.sh || exit 1
+umask 002
+cd /srv/grml-isos || exit 1
 for file in *.iso ; do
-    [ -f "${file}.md5" ] || md5sum $file > $file.md5
+    [ -f "${file}.md5" ] || md5sum $file > ${file}.md5
     chmod 664 $file
     chmod 664 ${file}.md5
 done
-umask 002
-rsync --partial -az --quiet /grml-live/grml-isos/* grml-build@debian.netcologne.de:/home/ftp/www.grml.org/daily/
-
-## END OF FILE #################################################################
+rsync --partial -az --quiet $ISO_DIR/* grml-build@debian.netcologne.de:/home/ftp/www.grml.org/daily/
