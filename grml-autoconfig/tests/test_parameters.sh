981e6fa5e07ab6ac9ec6ed1d805b9e82bf5dfd2f
Ulrich Dangel <uli@spamt.net>
2009-09-09 20:30:19 +0200

    simple unit tests for {get,check}bootparam

---
+++ grml-autoconfig/tests/test_parameters.sh
@@ -0,0 +1,64 @@
+#!/bin/zsh
+
+setopt shwordsplit
+
+test_checkbootparam() {
+    CMDLINE='foo=dingens bar foobar=blub'
+
+    checkbootparam foobar
+    retval=$?
+    assertEquals 'wrong retval for bootparam foobar' 0 ${retval}
+
+    checkbootparam bar
+    retval=$?
+    assertEquals 'wrong retval for bootparam bar' 0 ${retval}
+
+    checkbootparam foo
+    retval=$?
+    assertEquals 'wrong retval for bootparam foo' 0 ${retval}
+
+    checkbootparam dingens
+    retval=$?
+    assertEquals 'wrong retval for non-existing bootparam dingens' 1 ${retval}
+
+    checkbootparam oops
+    retval=$?
+    assertEquals 'wrong retval for non-existing bootparam oops' 1 ${retval}
+}
+
+test_getbootparam() {
+    CMDLINE='test=dingens tester foo=dingens foobar=blub'
+
+    value=$(getbootparam foobar)
+    retval=$?
+    assertEquals 'unexpected value for botparam foobar' 'blub' ${value}
+    assertEquals 'unexpected retval' '0' ${retval}
+
+    value=$(getbootparam foo)
+    retval=$?
+    assertEquals 'unexpected value for bootparam foo' 'dingens' ${value}
+    assertEquals 'unexpected retval' 0 ${retval}
+
+    value=$(getbootparam test)
+    retval=$?
+    assertEquals 'unexpected value for bootparam test' 'dingens' ${value}
+    assertEquals 'unexpected retval' 0 ${retval}
+
+    value=$(getbootparam tester)
+    retval=$?
+    assertTrue 'expected empty string' "[ -z ${value} ]"
+    assertEquals 'unexpected retval' 1 ${retval}
+}
+
+
+oneTimeSetUp() {
+    OLDPATH=$PATH
+
+    . ../autoconfig.functions
+
+    export PATH=$OLDPATH
+}
+
+SHUNIT_PARENT=$0
+
+. ./shunit2
