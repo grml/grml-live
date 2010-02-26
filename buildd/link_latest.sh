c3932cfd5462a702aa477dbd754dae5fe595ca78
Michael Prokop <mika@grml.org>
2010-02-26 21:33:50 +0100

    buildd/link_latest.sh: fix filename in md5 sum of latest ISO

--- buildd/link_latest.sh
+++ buildd/link_latest.sh
@@ -13,6 +13,8 @@ set -e
 [ -n "$MIRROR_DIRECTORY" ] || exit 1
 cd $MIRROR_DIRECTORY || exit 2
 
+DAILY_DIR=/srv/mirror/www.grml.org/daily
+
 cd "$DAILY_DIR"
 echo "---------------------------" >> "$DAILY_DIR"/.timestamp_link
 for flavour in grml-medium_lenny   grml-medium_squeeze   grml-medium_sid   grml-small_lenny   grml-small_squeeze  grml-small_sid \
@@ -20,10 +22,15 @@ for flavour in grml-medium_lenny   grml-medium_squeeze   grml-medium_sid   grml-
                grml64_lenny grml64_squeeze grml64_sid grml_lenny grml_squeeze grml_sid ; do
   ISO="$(ls -1 $flavour/*.iso | tail -1)"
   if [ -n "$ISO" ] ; then
-     ln -sf $ISO $(basename ${ISO%%_[0-9]*})_latest.iso
-     ln -sf ${ISO}.md5 $(basename ${ISO%%_[0-9]*})_latest.iso.md5
-     ln -sf ${ISO}.sha1 $(basename ${ISO%%_[0-9]*})_latest.iso.sha1
+     latest="$(basename ${ISO%%_[0-9]*})_latest.iso"
+     ln -sf $ISO ${latest}
+     # ln -sf ${ISO}.md5 ${latest}.md5
+     # http://bts.grml.org/grml/issue814
+     name=$(awk '{print $2}' "${ISO}".md5)
+     sed "s/$name/$latest/" "${ISO}".md5 > "${latest}".md5
+     echo "$ISO" >> "$DAILY_DIR"/.timestamp_link
   fi
 done
+echo "---------------------------" >> "$DAILY_DIR"/.timestamp_link
 
 ## END OF FILE #################################################################
