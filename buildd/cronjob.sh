cc7e99d97f3782f74f875a4482188334c70c844f
Michael Prokop <mika@grml.org>
2011-03-18 13:38:06 +0100

    buildd/cronjob.sh: upload and remove ISOS after each grml-flavour.

--- buildd/cronjob.sh
+++ buildd/cronjob.sh
@@ -7,12 +7,12 @@
 ################################################################################
 # Add something like that to the crontab to execute grml-live buildd
 # every day at a specific time:
-# 30 1 * * * /usr/share/grml-live/buildd/cronjob.sh
-# 
+# 50 0 * * * /usr/share/grml-live/buildd/cronjob.sh
+#
 # On the mirror (where you're hosting the ISOs) you might want to
 # install something like that in your crontab:
-# 02  * * * * /srv/mirror/www.grml.org/daily/.link_latest.sh
-# 03 03 * * * /srv/mirror/www.grml.org/daily/.cleanup.sh
+# 02  * * * * /home/mika/cronjobs/link_latest.sh
+# 03 03 * * * /home/mika/cronjobs/cleanup.sh
 ################################################################################
 
 if [ -r /usr/share/grml-live/buildd/buildd_running ] ; then
@@ -30,12 +30,18 @@ echo $$ > /usr/share/grml-live/buildd/buildd_running
 /usr/share/grml-live/buildd/grml-live_autobuild_grml-small_wheezy.sh && \
 /usr/share/grml-live/buildd/grml-live_autobuild_grml-small_sid.sh
 
+/usr/share/grml-live/buildd/upload_isos.sh && \
+/usr/share/grml-live/buildd/remove_isos.sh
+
 /usr/share/grml-live/buildd/grml-live_autobuild_grml64-medium_squeeze.sh && \
 /usr/share/grml-live/buildd/grml-live_autobuild_grml64-medium_wheezy.sh && \
 /usr/share/grml-live/buildd/grml-live_autobuild_grml64-medium_sid.sh && \
 /usr/share/grml-live/buildd/grml-live_autobuild_grml-medium_squeeze.sh && \
 /usr/share/grml-live/buildd/grml-live_autobuild_grml-medium_wheezy.sh && \
 /usr/share/grml-live/buildd/grml-live_autobuild_grml-medium_sid.sh
+
+/usr/share/grml-live/buildd/upload_isos.sh && \
+/usr/share/grml-live/buildd/remove_isos.sh
 
 /usr/share/grml-live/buildd/grml-live_autobuild_grml64-large_squeeze.sh && \
 /usr/share/grml-live/buildd/grml-live_autobuild_grml64-large_wheezy.sh && \
