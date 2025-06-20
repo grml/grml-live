55967a9d1e0e19aa080f39c88c50cf1973371bf0
Chris Hofstaedtler <ch@grml.org>
2025-06-20 23:47:53 +0200

    Enable grml-autoconfig shunit2 tests

--- tests/autoconfig/test_debs.sh
+++ tests/autoconfig/test_debs.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 # This test does not use shunit2 as shunit2 requires shwordsplit
 # But shwordsplit is not enabled per default in zsh and needs to be
 # taken care of in autoconfig.functions.
@@ -8,6 +8,8 @@ dpkg() {
     # remove the -i parameter
     shift
 
+    echo "dpkg params: $*"
+
     if [ $# -ne $EXPECTED_COUNT ] ; then
         echo "wrong parameter count for dpkg, was $# expected $EXPECTED_COUNT" >&2
         exit 1
@@ -15,15 +17,20 @@ dpkg() {
 }
 
 test_debs() {
-    . ../autoconfig.functions || exit 1
+    . ../../config/files/GRMLBASE/usr/share/grml-autoconfig/autoconfig.functions || exit 1
+
+    # shellcheck disable=SC2034  # used by autoconfig.functions
     CMDLINE='debs'
+    # shellcheck disable=SC2034  # used by autoconfig.functions
     INSTALLED=''
 
     TMPDIR=$(mktemp -d)
     DCSDIR="$TMPDIR"
     DEB_DIR="$DCSDIR"/debs
     mkdir -p "$DEB_DIR"
-    touch "$DEB_DIR"/{1..$EXPECTED_COUNT}.deb
+    for ((i=1; i <= "${EXPECTED_COUNT}"; i++)); do
+        touch "$DEB_DIR"/${i}.deb
+    done
 
     einfo() { echo "$*"; }
     eend() { echo ; }
