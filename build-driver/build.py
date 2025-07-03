grml/grml-live

commit 1f57a28765c9517f035e4d6a8b8fd3ec101c23b8

Drop support for all boot methods other than isolinux
The "grub2" and "manifold" options for HYBRID_METHOD as well as the
"grub" and "grub2" options for BOOT_METHOD are not used by any official
Grml ISO. But they add considerable overhead and complexity in our
code base.

I'd like to keep our code base simple and avoid feature creep, so let's
deprecate BOOT_METHOD, and drop support for HYBRID_METHOD other than
isolinux.

FTR: isolinux/syslinux is used for BIOS boot, GRUB is still relevant and
used for EFI boot.

etc/grml/grml-live.conf
@@ -92,10 +92,6 @@
 # Specify user with UID 1000 on live-system:
 # USERNAME=grml
 
-# Which bootloader do you want to use? Default: isolinux
-# Supported values: isolinux, grub, grub2
-# BOOT_METHOD='isolinux'
-
 # Directory of configuration files for grml-live's FAI:
 # GRML_FAI_CONFIG=/etc/grml/fai
 
@@ -133,8 +129,7 @@
 # likes the default directory [/usr/share/grml-live/templates]
 # TEMPLATE_DIRECTORY='/usr/share/grml-live/templates'
 
-# Use custom scripts, for example for bootgrub.mksh?
-# Useful when running grml-live from git checkout.
+# Use custom scripts, useful when running grml-live from git checkout.
 # If unset defaults to /usr/share/grml-live/scripts
 # SCRIPTS_DIRECTORY='/usr/share/grml-live/scripts'
 
@@ -152,12 +147,8 @@
 # boot the CD using normal el torito mode or copy it to USB device
 # *without* having to run grml2usb (like: 'dd if=grml.iso of=/dev/sdX')
 # - working both with the same ISO
-# Note that the manifold method is currently not be compatible with UEFI
-# setups.
 # HYBRID_METHOD='disable'   # do not create a hybrid ISO
 # HYBRID_METHOD='isohybrid' # use isohybrid from SYSLINUX (default)
-# HYBRID_METHOD='grub2'     # use manifold with GRUB 2
-# HYBRID_METHOD='manifold'  # use manifold with ISOLINUX
 
 # Secure Boot method that should be used (amd64 only).
 # If unset defaults to "disabled" (which means no Secure Boot will be present)

grml-live
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Filename:      grml-live
 # Purpose:       build process script for generating a (grml based) Linux Live-ISO
-# Authors:       grml-team (grml.org),
-#                (c) Michael Prokop <mika@grml.org>,
-#                (c) Thorsten Glaser <tg@mirbsd.org>
+# Authors:       Grml Team (see https://grml.org/)
 # Bug-Reports:   see http://grml.org/bugs/
 # License:       This file is licensed under the GPL v2 or any later version.
 ################################################################################
@@ -448,7 +446,6 @@ fi
 
 # assume sane defaults (if not set already) {{{
 [ -n "$ARCH" ]                    || ARCH="$(dpkg --print-architecture)"
-[ -n "$BOOT_METHOD" ]             || BOOT_METHOD='isolinux'
 [ -n "$CLASSES" ]                 || CLASSES="GRMLBASE,GRML_FULL,$(echo ${ARCH} | tr 'a-z' 'A-Z')"
 [ -n "$DATE" ]                    || DATE="$(date +%Y-%m-%d)"
 [ -n "$DISTRI_INFO" ]             || DISTRI_INFO='Grml - Live Linux for system administrators'
@@ -530,7 +527,6 @@ if [ -z "$FORCE" ] ; then
    [ -n "$VERSION" ]             && echo "  Grml version:      $VERSION"
    [ -n "$SUITE" ]               && echo "  Debian suite:      $SUITE"
    [ -n "$ARCH" ]                && echo "  Architecture:      $ARCH"
-   [ -n "$BOOT_METHOD" ]         && echo "  Boot method:       $BOOT_METHOD"
    [ -n "$HYBRID_METHOD" ]       && echo "  Hybrid method:     $HYBRID_METHOD"
    [ -n "$SECURE_BOOT" ]         && echo "  Secure Boot:       $SECURE_BOOT"
    [ -n "$TEMPLATE_DIRECTORY" ]  && echo "  Template files:    $TEMPLATE_DIRECTORY"
@@ -718,6 +714,14 @@ elif echo $CLASSES | grep -qi arm64 ; then
       eend 1
       bailout
    fi
+fi
+
+if [[ -n "${BOOT_METHOD:-}" ]] && [[ "${BOOT_METHOD}" != "isolinux" ]] ; then
+  log    "Error: You specified unsupported BOOT_METHOD '${BOOT_METHOD:-}', though only isolinux is supported."
+  eerror "Error: You specified unsupported BOOT_METHOD '${BOOT_METHOD:-}', though only isolinux is supported."
+  eerror "NOTE:  The BOOT_METHOD configuration is deprecated, please consider unsetting it. Exiting."
+  eend 1
+  bailout
 fi
 
 # generate nfsroot configuration for FAI on the fly
@@ -1535,7 +1539,6 @@ fi
 # shellcheck disable=SC2034
 generate_build_info() {
   jo -p \
-    boot_method="${BOOT_METHOD}" \
     bootstrap_only="${BOOTSTRAP_ONLY}" \
     build_date="${DATE}" \
     build_dirty="${BUILD_DIRTY}" \
@@ -1594,21 +1597,9 @@ generate_build_info() {
 [ -n "$ISO_OUTPUT" ] || ISO_OUTPUT="$OUTPUT/grml_isos"
 [ -n "$ISO_NAME" ] || ISO_NAME="${GRML_NAME}_${VERSION}.iso"
 
-if [ "$BOOT_METHOD" = "isolinux" ] ; then
-   BOOT_ARGS="-no-emul-boot -boot-load-size 4 -boot-info-table -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat"
-   if [ "$HYBRID_METHOD" = "isohybrid" ] ; then
-     EFI_ARGS="-isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -eltorito-alt-boot -e boot/efi.img -no-emul-boot -isohybrid-gpt-basdat"
-   fi
-elif [ "$BOOT_METHOD" = "grub2" ] ; then
-   BOOT_ARGS="-no-emul-boot -boot-load-size 4 -b boot/grub/toriboot.bin"
-fi
-
-# Work around http://bts.grml.org/grml/issue945
-if [[ $BOOT_METHOD != isolinux && ($HYBRID_METHOD = isohybrid || $HYBRID_METHOD = manifold) ]]; then
-  log   "Setting HYBRID_METHOD to grub2 as hybrid mode does not work with isohybrid yet."
-  ewarn "Setting HYBRID_METHOD to grub2 as hybrid mode does not work with isohybrid yet."
-  HYBRID_METHOD='grub2'
-  eend 0
+BOOT_ARGS="-no-emul-boot -boot-load-size 4 -boot-info-table -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat"
+if [ "$HYBRID_METHOD" = "isohybrid" ] ; then
+  EFI_ARGS="-isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -eltorito-alt-boot -e boot/efi.img -no-emul-boot -isohybrid-gpt-basdat"
 fi
 
 if [ -f "${ISO_OUTPUT}/${ISO_NAME}" -a -z "$UPDATE" -a -z "$BUILD_ONLY" -a -z "$BUILD_DIRTY" -a "$FORCE_ISO_REBUILD" = "false" ]  ; then
@@ -1665,14 +1656,6 @@ else
 
    CURRENT_DIR=$(pwd)
    if cd "$BUILD_OUTPUT" ; then
-      if [ "$BOOT_METHOD" = "grub2" ]; then
-         # make a 2048-byte bootsector for El Torito
-         dd if=/dev/zero of=boot/grub/toriboot.bin bs=512 count=4 2>/dev/null
-         # those are in 2048-byte sectors, so 1 16 matches 4 63 below
-         echo 1 16 | mksh "${SCRIPTS_DIRECTORY}/bootgrub.mksh" -B 11 | \
-            dd of=boot/grub/toriboot.bin conv=notrunc 2>/dev/null
-      fi
-
       log   "Generating build information in conf/buildinfo.json"
       einfo "Generating build information in conf/buildinfo.json"
       mkdir -p conf/
@@ -1693,56 +1676,12 @@ else
         bailout $RC
       fi
 
-      # both of these need core.img there, so it’s easier to write it here
-      if [ "$BOOT_METHOD" = "grub2" ] || [ "$HYBRID_METHOD" = "grub2" ]; then
-         # must be <= 30720 bytes
-         dd if=boot/grub/core.img of="${ISO_OUTPUT}/${ISO_NAME}" \
-           conv=notrunc bs=512 seek=4 2>/dev/null
-      fi
-
       # pad the output ISO to multiples of 256 KiB for partition table support
       siz=$($getfilesize "${ISO_OUTPUT}/${ISO_NAME}")
       cyls=$((siz / 512 / 32 / 16 + 1))   # C=$cyls H=16 S=32
       siz=$((cyls * 16 * 32 * 512))   # size after padding
       dd if=/dev/zero bs=1 count=1 seek=$((siz - 1)) \
          of="${ISO_OUTPUT}/${ISO_NAME}" 2>/dev/null
-
-      # support disabling hybrid ISO image
-      if [ "$HYBRID_METHOD" = "disable" ] ; then
-        log   "Skipping creation of hybrid ISO file as requested via HYBRID_METHOD=disable"
-        einfo "Skipping creation of hybrid ISO file as requested via HYBRID_METHOD=disable"
-        eend 0
-      elif [ "$HYBRID_METHOD" = "manifold" ] || [ "$HYBRID_METHOD" = "grub2" ] ; then
-        # isoinfo is part of both mkisofs and genisoimage so we're good
-        bootoff=$(isoinfo -l -i "${ISO_OUTPUT}/${ISO_NAME}" | \
-          sed -n '/^.*\[ *\([0-9]*\)[] ].* ISOLINUX.BIN[;1]* *$/s//\1/p')
-
-        if ! [ -r boot/grub/core.img ] ; then
-          log   "boot/grub/core.img not found, not creating manifold boot ISO file"
-          ewarn "boot/grub/core.img not found, not creating manifold boot ISO file"
-        elif [ "${bootoff:-0}" -lt 1 ] ; then
-          log   "isolinux.bin not found on the ISO file, disabling manifold boot"
-          ewarn "isolinux.bin not found on the ISO file, disabling manifold boot"
-        else
-          if [ "$HYBRID_METHOD" = "grub2" ] ; then
-            log   "Creating hybrid ISO file with manifold/grub2 method"
-            einfo "Creating hybrid ISO file with manifold/grub2 method"
-            # 512 bytes: MBR, partition table, load GRUB 2
-            echo 4 63 | mksh "${SCRIPTS_DIRECTORY}/bootgrub.mksh" -A -M 4:0x96 -g $cyls:16:32
-          else
-            log   "Creating hybrid ISO file with manifold method"
-            einfo "Creating hybrid ISO file with manifold method"
-            # read only one but 2048-byte sized (scale: << 2) sector
-            echo $bootoff $bootoff | \
-              mksh ${SCRIPTS_DIRECTORY}/bootilnx.mksh -A -M 4:0x96 -g $cyls:16:32 -S 2
-          fi | dd of="${ISO_OUTPUT}/${ISO_NAME}" conv=notrunc 2>/dev/null
-          eend $?
-        fi
-      elif [ "$HYBRID_METHOD" = "isohybrid" ] ; then
-        : # nothing to do, handled via $MKISOFS $EFI_ARGS already
-      else
-        bailout 12 "Unknown HYBRID_METHOD [${HYBRID_METHOD}]. Supported values: disable, isohybrid, grub2, manifold"
-      fi
 
       # generate ISO checksums if we are using class 'RELEASE':
       case $CLASSES in *RELEASE*)

debian/control
@@ -25,7 +25,6 @@ Depends:
  fai-client (>= 3.4.0),
  isolinux (>= 3:6.03+dfsg-5+deb8u1~),
  jo,
- mksh,
  moreutils,
  mtools,
  pciutils,

debian/copyright
@@ -11,10 +11,6 @@ Files: fonts/graphicoreBitmapFont0-Light.otf
 Copyright: 2010, Lasse Fister lasse@graphicore.de
 License: SIL
 
-Files: scripts/bootgrub.mksh scripts/bootilnx.mksh
-Copyright: 2007, 2008, 2009, 2010 Thorsten Glaser <tg@mirbsd.org>
-License: MirOS
-
 Files: templates/EFI/debian/BOOT/grubx64.efi.signed
 Copyright: 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009  Free Software Foundation, Inc
            2009-2012 Intel Corporation

debian/grml-live.lintian-overrides
@@ -1,8 +1,6 @@
 grml-live: bugs-field-does-not-refer-to-debian-infrastructure mailto:bugs@grml.org
 grml-live: executable-not-elf-or-script usr/share/grml-live/templates/windows/autostart/autorun.inf
 grml-live: privacy-breach-generic usr/share/grml-live/templates/GRML/index.html [<link rel="home" href="http://grml.org/" title="grml.org" />] (http://grml.org/)
-grml-live: script-not-executable usr/share/grml-live/scripts/bootgrub.mksh
-grml-live: script-not-executable usr/share/grml-live/scripts/bootilnx.mksh
 grml-live: unknown-section grml
 grml-live: uses-dpkg-database-directly etc/grml/fai/config/hooks/instsoft.GRMLBASE
 grml-live: uses-dpkg-database-directly etc/grml/fai/config/scripts/GRMLBASE/98-clean-chroot

remaster/grml-live-remaster
@@ -174,8 +174,6 @@ siz=$((cyls * 16 * 32 * 512))           # size after padding
 dd if=/dev/zero bs=1 count=1 seek=$((siz - 1)) of="$1" 2>/dev/null
 rm -R /remaster/iso
 
-# here is the place where we could apply bootgrub.mksh
-
 echo ""
 echo "ISO generation complete:"
 ls --color -l "$1"

scripts/bootgrub.mksh
@@ -1,281 +1,0 @@
-#!/usr/bin/env mksh
-# $Id$
-# $miros: src/sys/arch/i386/stand/bootxx/mkbxinst.sh,v 1.28 2010/11/12 21:20:35 tg Exp $
-# $miros: src/sys/arch/i386/stand/bootxx/bootxx.S,v 1.26 2010/12/01 19:56:58 tg Exp $ +t:GRUB +s:MBR
-#-
-# Copyright (c) 2007, 2008, 2009, 2010
-#	Thorsten Glaser <tg@mirbsd.org>
-#
-# Provided that these terms and disclaimer and all copyright notices
-# are retained or reproduced in an accompanying document, permission
-# is granted to deal in this work without restriction, including un‐
-# limited rights to use, publicly perform, distribute, sell, modify,
-# merge, give away, or sublicence.
-#
-# This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
-# the utmost extent permitted by applicable law, neither express nor
-# implied; without malicious intent or gross negligence. In no event
-# may a licensor, author or contributor be held liable for indirect,
-# direct, other damage, loss, or other issues arising in any way out
-# of dealing in the work, even if advised of the possibility of such
-# damage or existence of a defect, except proven that it results out
-# of said person’s immediate fault when using the work as intended.
-#-
-# Self-installing 32-bit x86 boot blocks for GNU GRUB2 on i386-pc
-# Reads a list of extents (firstblock lastblock) from standard input
-# and writes bootxx to standard output, which can subsequentially be
-# stored as partition boot record (or floppy boot sector) on disc.
-
-set -A thecode 0x66 0x31 0xC9 0x8E 0xD1 0xBC 0xFC 0x7B 0x66 0x51 0x66 0x9D 0x8E 0xC1 0xBE 0x00 0x7C 0x8E 0xD9 0xBB 0x00 0x08 0x53 0x53 0x8E 0xC3 0xBF 0x00 0xFE 0xB5 0x02 0xF3 0xA4 0x1F 0x68 0x6B 0xFE 0xCB 0x30 0x41 0x41 0x44 0x20 0x4C 0x6F 0x61 0x64 0x69 0x6E 0x67 0x20 0x00 0x20 0x65 0x72 0x72 0x6F 0x72 0x0D 0x0A 0x00 0x96 0x02 0x00 0x12 0x00 0x00 0x80 0x65 0xFF 0xB4 0x0E 0xBB 0x07 0x00 0xCD 0x10 0xAC 0x08 0xC0 0x75 0xF4 0xC3 0xE8 0xF7 0xFF 0xB8 0x41 0xFF 0x87 0x06 0x44 0xFE 0x3D 0x41 0xFF 0x75 0x19 0x31 0xC0 0xCD 0x16 0xEA 0xF0 0xFF 0x00 0xF0 0xFB 0x88 0x16 0x43 0xFE 0xBE 0x26 0xFE 0x80 0xFA 0x80 0x72 0xDB 0xE8 0xD2 0xFF 0x31 0xDB 0xBE 0x68 0xFF 0x0F 0xB6 0x2E 0x3D 0xFE 0xAC 0x0F 0xB6 0xC8 0xC0 0xE9 0x05 0x83 0xE0 0x1F 0x40 0x41 0x50 0xBF 0x10 0xFE 0xB8 0x10 0x00 0xAB 0xB0 0x01 0xAB 0x89 0xD8 0xAB 0x8C 0xC8 0xAB 0xF3 0xA4 0x31 0xC0 0xAB 0xAB 0xAB 0xAB 0x5F 0x55 0x56 0xBE 0x10 0xFE 0xFF 0x16 0x44 0xFE 0xBD 0x04 0x00 0x8A 0x16 0x43 0xFE 0x60 0xF9 0xCD 0x13 0x9C 0xB8 0x2E 0x0E 0xBB 0x07 0x00 0xCD 0x10 0x9D 0xFB 0x61 0x73 0x1C 0x4D 0x60 0x9C 0x31 0xC0 0xCD 0x13 0x9D 0xBE 0x34 0xFE 0x0F 0x84 0x77 0xFF 0xB8 0x30 0x0E 0x01 0xE8 0xBB 0x07 0x00 0xCD 0x10 0x61 0xEB 0xD2 0xB4 0x02 0x00 0xE7 0x89 0x5C 0x04 0x00 0xFC 0x72 0x14 0x66 0x83 0x44 0x08 0x01 0x66 0x83 0x54 0x0C 0x00 0x4F 0x75 0xAC 0x5E 0x5D 0x4D 0x0F 0x85 0x7D 0xFF 0xBE 0x3A 0xFE 0xE8 0x3F 0xFF 0x66 0x31 0xD2 0x52 0x66 0x4A 0x8A 0x16 0x43 0xFE 0xB8 0x00 0x82 0x50 0xFA 0xCB 0x60 0x06 0xB4 0x08 0x8A 0x16 0x43 0xFE 0xF9 0xCD 0x13 0xFB 0x07 0xBE 0x34 0xFE 0x0F 0x82 0x21 0xFF 0x80 0xE1 0x3F 0x88 0x0E 0x40 0xFE 0x0F 0xB6 0xC6 0x40 0xA3 0x3E 0xFE 0x61 0x8B 0x0E 0x40 0xFE 0xE3 0xD7 0x8B 0x44 0x08 0x8B 0x54 0x0A 0xF7 0xF1 0x42 0x31 0xC9 0x87 0xD1 0xF7 0x36 0x3E 0xFE 0xC0 0xE4 0x06 0x86 0xC4 0x09 0xC1 0x88 0xD6 0xB8 0x01 0x02 0xC3 0xB4 0x42 0xC3
-typeset -i ofs_bkcnt=61
-typeset -i ofs_geomh=62
-typeset -i ofs_geoms=64
-typeset -i ofs_partp=66
-typeset -i ofs_secsz=234
-typeset -i begptr=360
-typeset -Uui8 thecode
-
-typeset -Uui16 curptr=begptr
-typeset -i wnum=0 wofs=0 wrec=0 bkend=0x1FE
-
-function do_record {
-	typeset -Ui blk=$1 cnt=$2 n
-	typeset -Uui16 x=blk y
-
-	(( blk && cnt )) || return
-
-	print -u2 "$wrec @0x${curptr#16#}: $cnt @$blk (0x${x#16#})"
-
-	while (( cnt )); do
-		let wrec++
-		(( n = blk < 0x00000100 ? 0 :
-		    blk < 0x00010000 ? 1 :
-		    blk < 0x01000000 ? 2 : 3 ))
-		(( x = cnt < 33 ? cnt : 32 ))
-		(( y = blk ))
-		print -u2 " - 0x${curptr#16#}: $((x)) (0x${x#16#}) @ $blk" \
-		    "(0x${y#16#})"
-		(( thecode[curptr++] = (n++ << 5) | (x - 1) ))
-		(( blk += x ))
-		(( cnt -= x ))
-		while (( n-- )); do
-			(( thecode[curptr++] = y & 0xFF ))
-			(( y >>= 8 ))
-		done
-	done
-}
-
-function record_block {
-	typeset -Ui blk=$1
-
-	if (( !blk || (wofs && blk != (wofs + wnum)) )); then
-		# flush the blocks from the cache
-		(( wnum )) && do_record $wofs $wnum
-		wofs=0
-		wnum=0
-	fi
-	if (( blk )); then
-		# record some new block into the cache
-		(( wofs )) || let wofs=blk
-		(( wnum += 1 << sscale ))
-	fi
-}
-
-typeset -i partp=0 numheads=0 numsecs=0 sscale=0 bsh=9 mbrpno=0 mbrptp=0 pofs=0
-set -A g_code 0 0 0
-
-while getopts ":0:1AB:g:h:M:O:p:S:s:" ch; do
-	case $ch {
-	(0)	;;
-	(1)	;;
-	(A)	numheads=0
-		numsecs=99
-		;;
-	(B)	if (( (bsh = OPTARG) < 9 || OPTARG > 15 )); then
-			print -u2 Error: invalid block size "2^'$OPTARG'"
-			exit 1
-		fi
-		;;
-	(g)	if [[ $OPTARG != +([0-9]):+([0-9]):+([0-9]) ]]; then
-			print -u2 Error: invalid geometry code "'$OPTARG'"
-			exit 1
-		fi
-		saveIFS=$IFS
-		IFS=:
-		set -A g_code -- $OPTARG
-		IFS=$saveIFS ;;
-	(h)	if (( (numheads = OPTARG) < 1 || OPTARG > 256 )); then
-			print -u2 Warning: invalid head count "'$OPTARG'"
-			numheads=0
-		fi ;;
-	(M)	if [[ $OPTARG != +([0-9])?(:?(0[Xx])+([0-9])) ]]; then
-			print -u2 Warning: invalid partition info "'$OPTARG'"
-			mbrpno=0
-			mbrptp=0
-		else
-			saveIFS=$IFS
-			IFS=:
-			set -A mbr_code -- $OPTARG
-			IFS=$saveIFS
-			(( mbrpno = mbr_code[0] ))
-			(( mbrptp = mbr_code[1] ))
-			if (( mbrpno < 1 || mbrpno > 4 )); then
-				print -u2 Warning: invalid partition \
-				    number "'$OPTARG'"
-				mbrpno=0
-			fi
-			if (( mbrptp < 1 || mbrptp > 255 )); then
-				print -u2 Warning: invalid partition \
-				    type "'$OPTARG'"
-				mbrptp=0
-			fi
-		fi ;;
-	(O)	if [[ $OPTARG != +([0-9]) ]]; then
-			print -u2 Warning: invalid partition offset "'$OPTARG'"
-		else
-			pofs=$OPTARG
-		fi ;;
-	(p)	if (( (partp = OPTARG) < 1 || OPTARG > 255 )); then
-			print -u2 Warning: invalid partition type "'$OPTARG'"
-			partp=0
-		fi ;;
-	(S)	if (( (sscale = OPTARG) < 0 || OPTARG > 24 )); then
-			print -u2 Error: invalid input scale "'$OPTARG'"
-			exit 1
-		fi ;;
-	(s)	if (( (numsecs = OPTARG) < 1 || OPTARG > 63 )); then
-			print -u2 Warning: invalid sector count "'$OPTARG'"
-			numsecs=0
-		fi ;;
-	(*)	print -u2 'Syntax:
-	bxinst [-1A] [-B blocksize] [-g C:H:S] [-h heads] [-M pno(1..4)[:typ]]
-	    [-O partitionofs] [-p type] [-S scale] [-s secs] <sectorlist | \\
-	    dd of=image conv=notrunc
-Default values: blocksize=9 heads=16 sectors=63 part.ofs=0 type=0x27 scale=0
-    partno=4 if -g (create MBR partition) is given; -A = auto boot geometry'
-		exit 1 ;;
-	}
-done
-shift $((OPTIND - 1))
-
-typeset -Ui psz=0	# must be unsigned
-if (( g_code[0] )); then
-	# bounds check partition table values, calculate total sectors
-	if (( g_code[0] < 1 || g_code[1] < 1 || g_code[1] > 256 ||
-	    g_code[2] < 1 || g_code[2] > 63 )); then
-		print -u2 Invalid geometry, values out of bounds.
-	elif [[ $(print "(${g_code[0]} * ${g_code[1]} * ${g_code[2]})" \
-	    "> 4294967295" | bc) = 1 ]]; then
-		print -u2 Invalid geometry, more than 2 TiB of data.
-	else
-		# we know it's <= 2^32-1
-		(( psz = g_code[0] * g_code[1] * g_code[2] ))
-	fi
-fi
-if (( psz )); then
-	print -u2 geometry is $psz sectors \($(print \
-	    "$psz * $((1 << bsh))" | bc) bytes\) in ${g_code[0]} cylinders, \
-	    ${g_code[1]} heads, ${g_code[2]} sectors per track
-	if (( numsecs == 0 || (numsecs != 99 && numheads == 0) )); then
-		print -u2 Warning: using these values for C/H/S boot
-		numheads=${g_code[1]}
-		numsecs=${g_code[2]}
-	fi
-	(( mbrpno )) || mbrpno=4	# default partition number
-fi
-if (( mbrpno )); then
-	bkend=0x1BE
-	(( psz )) || print -u2 Warning: no geometry given, will not \
-	    create an MBR partition table entry
-fi
-
-if (( numsecs == 99 )); then
-	numheads=0
-	numsecs=0
-else
-	if (( !numheads )); then
-		print -u2 Warning: using default value of 16 heads
-		numheads=16
-	fi
-
-	if (( !numsecs )); then
-		print -u2 Warning: using default value of 63 sectors
-		numsecs=63
-	fi
-fi
-
-# read in the extents
-while read firstblock lastblock junk; do
-	while (( firstblock <= lastblock )); do
-		record_block $((firstblock++ << sscale))
-	done
-done
-record_block 0	# just flush
-print -u2 "using $wrec blocks, $((curptr-begptr)) bytes ($((bkend-curptr)) free)"
-
-# fill the block table
-if (( curptr-- > bkend )); then
-	print -u2 Error: too many blocks
-	exit 1
-fi
-while (( ++curptr < bkend )); do
-	(( thecode[curptr] = (curptr & 0xFCF) == 0x1C2 ? 0 : RANDOM & 0xFF ))
-	# ensure the “active” flag is never set to 0x00 or 0x80
-	if (( ((curptr + 2) & 0xFCF) == 0x01C0 )); then
-		(( thecode[curptr] & 0x7F )) || let --curptr
-	fi
-done
-thecode[510]=0x55
-thecode[511]=0xAA
-
-# fill in other data
-(( thecode[ofs_bkcnt] = wrec ))
-(( thecode[ofs_geomh] = numheads & 0xFF ))
-(( thecode[ofs_geomh + 1] = numheads >> 8 ))
-(( thecode[ofs_geoms] = numsecs ))
-(( thecode[ofs_partp] = partp ))
-print -u2 "using sectors of 2^$bsh = $((1 << bsh)) bytes"
-(( thecode[ofs_secsz] = (1 << (bsh - 8)) ))
-
-# create an MBR partition if desired
-if (( psz )); then
-	(( mbrpno = 0x1BE + ((mbrpno - 1) * 16) ))
-	set -A o_code	# g_code equivalent for partition offset
-	(( o_code[2] = pofs % g_code[2] + 1 ))
-	(( o_code[1] = pofs / g_code[2] ))
-	(( o_code[0] = o_code[1] / g_code[1] + 1 ))
-	(( o_code[1] = o_code[1] % g_code[1] + 1 ))
-	# boot flag; C/H/S offset
-	thecode[mbrpno++]=0x80
-	(( thecode[mbrpno++] = o_code[1] - 1 ))
-	(( cylno = o_code[0] > 1024 ? 1023 : o_code[0] - 1 ))
-	(( thecode[mbrpno++] = o_code[2] | ((cylno & 0x0300) >> 2) ))
-	(( thecode[mbrpno++] = cylno & 0x00FF ))
-	# partition type; C/H/S end
-	(( thecode[mbrpno++] = (mbrptp ? mbrptp : partp ? partp : 0x27) ))
-	(( thecode[mbrpno++] = g_code[1] - 1 ))
-	(( cylno = g_code[0] > 1024 ? 1023 : g_code[0] - 1 ))
-	(( thecode[mbrpno++] = g_code[2] | ((cylno & 0x0300) >> 2) ))
-	(( thecode[mbrpno++] = cylno & 0x00FF ))
-	# partition offset, size (LBA)
-	(( thecode[mbrpno++] = pofs & 0xFF ))
-	(( thecode[mbrpno++] = (pofs >> 8) & 0xFF ))
-	(( thecode[mbrpno++] = (pofs >> 16) & 0xFF ))
-	(( thecode[mbrpno++] = (pofs >> 24) & 0xFF ))
-	(( pssz = psz - pofs ))
-	(( thecode[mbrpno++] = pssz & 0xFF ))
-	(( thecode[mbrpno++] = (pssz >> 8) & 0xFF ))
-	(( thecode[mbrpno++] = (pssz >> 16) & 0xFF ))
-	(( thecode[mbrpno++] = (pssz >> 24) & 0xFF ))
-fi
-
-# create the output string
-ostr=
-curptr=0
-while (( curptr < 512 )); do
-	ostr=$ostr\\0${thecode[curptr++]#8#}
-done
-
-# over and out
-print -n "$ostr"
-exit 0

commit 1d6aed43ddfd3ff27c740f9372749847e1c0ce9f

grml-live: deprecate -S/SCRIPTS_DIRECTORY
Unused since commit 1f57a28765c9517f035e4d6a8b8fd3ec101c23b8.

Fixes: commit 1f57a28765c9517f035e4d6a8b8fd3ec101c23b8

build-driver/build.py
@@ -106,7 +106,6 @@ def run_grml_live(
     env.update(
         {
             "GRML_FAI_CONFIG": str(grml_fai_config),
-            "SCRIPTS_DIRECTORY": str(grml_live_path / "scripts"),
             "LIVE_CONF": str(grml_live_path / "etc" / "grml" / "grml-live.conf"),
             "TEMPLATE_DIRECTORY": str(grml_live_path / "templates"),
             "SOURCE_DATE_EPOCH": str(int(source_date_epoch.timestamp())),

etc/grml/grml-live.conf
@@ -106,10 +106,6 @@
 # likes the default directory [/usr/share/grml-live/templates]
 # TEMPLATE_DIRECTORY='/usr/share/grml-live/templates'
 
-# Use custom scripts, useful when running grml-live from git checkout.
-# If unset defaults to /usr/share/grml-live/scripts
-# SCRIPTS_DIRECTORY='/usr/share/grml-live/scripts'
-
 # Do you want to use console based isolinux boot splash
 # instead of the default graphical menu?
 # ISOLINUX_METHOD="console"

grml-live
@@ -64,7 +64,7 @@ Usage: $PN [options, see as follows]
    -r <release_name>       release name
    -R                      skip applying RELEASE clean up
    -s <suite>              Debian suite/release, like: stable, testing, unstable
-   -S <script_directory>   place of scripts (defaults to /usr/share/grml-live/scripts)
+   -S <arg>                deprecated - will be removed from grml-live
    -t <template_directory> place of the templates
    -u                      update existing chroot instead of rebuilding it from scratch
    -U <username>           arrange output to be owned by specified username
@@ -332,7 +332,7 @@ while getopts "a:C:c:d:D:e:g:i:I:o:r:s:S:t:U:v:w:AbBFhnNqQRuVz" opt; do
     r) RELEASENAME="$OPTARG" ;;
     R) SKIP_RELEASE=1 ;;
     s) SUITE="$OPTARG" ;;
-    S) SCRIPTS_DIRECTORY="$OPTARG";;
+    S) echo "grml-live option -S is deprecated, please remove it." ;;
     t) TEMPLATE_DIRECTORY="$OPTARG";;
     v) VERSION="$OPTARG" ;;
     F) FORCE=1 ;;
@@ -405,7 +405,6 @@ export SOURCE_DATE_EPOCH
 [ -n "$SQUASHFS_EXCLUDES_FILE" ]  || SQUASHFS_EXCLUDES_FILE="${GRML_FAI_CONFIG}/grml/squashfs-excludes"
 [ -n "$SUITE" ]                   || SUITE='testing'
 [ -n "$TEMPLATE_DIRECTORY" ]      || TEMPLATE_DIRECTORY='/usr/share/grml-live/templates'
-[ -n "$SCRIPTS_DIRECTORY" ]       || SCRIPTS_DIRECTORY='/usr/share/grml-live/scripts'
 [ -n "$USERNAME" ]                || USERNAME='grml'
 [ -n "$VERSION" ]                 || VERSION='0.0.1'
 
@@ -1474,7 +1473,6 @@ generate_build_info() {
     grml_iso_output="${ISO_OUTPUT}" \
     grml_live_cmdline="${CMDLINE}" \
     grml_live_config_file="${LIVE_CONF}" \
-    grml_live_scripts_directory="${SCRIPTS_DIRECTORY}" \
     grml_live_template_directory="${TEMPLATE_DIRECTORY}" \
     grml_live_version="${GRML_LIVE_VERSION}" \
     grml_local_config="${LOCAL_CONFIG}" \