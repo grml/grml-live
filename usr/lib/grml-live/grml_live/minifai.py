4648e6ededaf768d08ebaa1f46efcae456651039
Chris Hofstaedtler <ch@grml.org>
2026-06-25 18:36:00 +0200

    grml-live: expose ISO_NAME to minifai

--- usr/lib/grml-live/grml_live/minifai.py
+++ usr/lib/grml-live/grml_live/minifai.py
@@ -833,6 +833,8 @@ def _run_tasks(
 
     grml_name = os.environ["GRML_NAME"]
     print(f"I: GRML_NAME: {grml_name!r}")
+    iso_name = os.environ["ISO_NAME"]
+    print(f"I: ISO_NAME: {iso_name!r}")
 
     do_skiptask(dynamic_state, skip_tasks)
 
