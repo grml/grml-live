grml/grml-live

commit d46f91b633aa8ff424ea9a7559c40d76da252155

grml-live: introduce media-scripts
These will be used to customize the ISO (not chroot/squashfs!) contents.

usr/lib/grml-live/grml_live/minifai.py
@@ -33,6 +33,7 @@ class FaiAction(StrEnum):
     DIRINSTALL = "dirinstall"
     SOFTUPDATE = "softupdate"
     RECONFIGURE = "reconfigure"
+    REBUILD = "rebuild"
 
 
 @dataclass
@@ -58,6 +59,10 @@ class BuildDirectories:
     build_dir: Path
     log_dir_inside: str
     log_dir: Path
+    netboot_dir_inside: str
+    netboot_dir: Path
+    media_dir_inside: str
+    media_dir: Path
     sources_dir_inside: str
     sources_dir: Path
 
@@ -159,11 +164,18 @@ def run_script(chroot_dir: Path, script: Path, helper_tools_path: Path, env: dic
     print(f"I: Finished script {script}.")
 
 
-def run_class_scripts(conf_dir: Path, chroot_dir: Path, class_name: str, helper_tools_path: Path, env: dict[str, str]):
+def run_class_scripts(
+    script_type: str,
+    conf_dir: Path,
+    chroot_dir: Path,
+    class_name: str,
+    helper_tools_path: Path,
+    env: dict[str, str],
+):
     print()
-    print(f'I: Running "scripts" for class {class_name}...')
+    print(f'I: Running "{script_type}" for class {class_name}...')
     print()
-    scripts_dir = conf_dir / "scripts" / class_name
+    scripts_dir = conf_dir / script_type / class_name
     for script in sorted(scripts_dir.glob("*")):
         if script.name.endswith(".dpkg-old") or script.name.endswith(".dpkg-new"):
             print(f"W: Skipping {script} due to name suffix, please delete it")
@@ -610,6 +622,14 @@ def _create_dirs(chroot_dir: Path) -> BuildDirectories:
     log_dir = build_dir / log_dir_name
     log_dir.mkdir()
 
+    media_dir_name = "media"
+    media_dir = build_dir / media_dir_name
+    media_dir.mkdir()
+
+    netboot_dir_name = "netboot"
+    netboot_dir = build_dir / netboot_dir_name
+    netboot_dir.mkdir()
+
     sources_dir_name = "grml_sources"
     sources_dir = build_dir / sources_dir_name
     sources_dir.mkdir()
@@ -619,6 +639,10 @@ def _create_dirs(chroot_dir: Path) -> BuildDirectories:
         build_dir=build_dir,
         log_dir_inside=f"/{build_dir_relative}/{log_dir_name}/",
         log_dir=log_dir,
+        media_dir_inside=f"/{build_dir_relative}/{media_dir_name}/",
+        media_dir=media_dir,
+        netboot_dir_inside=f"/{build_dir_relative}/{netboot_dir_name}/",
+        netboot_dir=media_dir,
         sources_dir_inside=f"/{build_dir_relative}/{sources_dir_name}/",
         sources_dir=sources_dir,
     )
@@ -644,6 +668,8 @@ def _run_tasks(
     env = {
         "GRML_LIVE_CONFIG": str(grml_live_config_chroot),
         "GRML_LIVE_BUILDDIR": directories.build_dir_inside,
+        "GRML_LIVE_MEDIADIR": directories.media_dir_inside,
+        "GRML_LIVE_NETBOOTDIR": directories.netboot_dir_inside,
         "GRML_LIVE_SOURCESDIR": directories.sources_dir_inside,
         "LOGDIR": str(directories.log_dir),
     } | read_envvars_for_classes(conf_dir, classes)
@@ -662,7 +688,11 @@ def _run_tasks(
 
         if not should_skip_task(dynamic_state, "configure"):
             for class_name in classes:
-                run_class_scripts(conf_dir, chroot_dir, class_name, helper_tools_path, env)
+                run_class_scripts("scripts", conf_dir, chroot_dir, class_name, helper_tools_path, env)
+
+        if not should_skip_task(dynamic_state, "build"):
+            for class_name in classes:
+                run_class_scripts("media-scripts", conf_dir, chroot_dir, class_name, helper_tools_path, env)
 
     return 0
 
@@ -713,6 +743,15 @@ def _main(program_name: str, argv: list[str]) -> int:
         elif args.action == FaiAction.RECONFIGURE:
             rc = _run_tasks(
                 conf_dir, chroot_dir, classes, args.grml_live_config, args.action, ["updatebase", "instsoft"]
+            )
+        elif args.action == FaiAction.REBUILD:
+            rc = _run_tasks(
+                conf_dir,
+                chroot_dir,
+                classes,
+                args.grml_live_config,
+                args.action,
+                ["updatebase", "instsoft", "configure"],
             )
         else:
             print(f"E: minifai: Unknown fai action: {args.action!r}")

grml-live
@@ -775,7 +775,7 @@ fi
 # }}}
 
 # CHROOT_OUTPUT - execute FAI {{{
-if [ -n "$BUILD_DIRTY" ]; then
+if false ; then
    log   "Skipping stage 'fai' as requested via option -B"
    ewarn "Skipping stage 'fai' as requested via option -B" ; eend 0
 else
@@ -785,20 +785,25 @@ else
       FAI_ACTION=reconfigure
    elif [ -n "$UPDATE" ] ; then
       FAI_ACTION=softupdate
+   elif [ -n "$BUILD_DIRTY" ] ; then
+      FAI_ACTION=rebuild
+   elif [ -d "$CHROOT_OUTPUT/bin" ] ; then
+      log   "Skipping chroot bootstrap as $CHROOT_OUTPUT exists already."
+      ewarn "Skipping chroot bootstrap as $CHROOT_OUTPUT exists already." ; eend 0
    else
       FAI_ACTION=dirinstall
    fi
 
-   if [ -n "$UPDATE" ] || [ -n "$BUILD_ONLY" ] ; then
+   if [ -n "$UPDATE" ] || [ -n "$BUILD_ONLY" ] || [ -n "$BUILD_DIRTY" ] ; then
       if ! [ -r "$CHROOT_OUTPUT/etc/debian_version" ] ; then
          log    "Error: does not look like you have a working chroot. Updating/building not possible."
-         eerror "Error: does not look like you have a working chroot. Updating/building not possible. (Drop -u/-b option?)"
+         eerror "Error: does not look like you have a working chroot. Updating/building not possible. (Drop -u/-b/-B option?)"
          eend 1
          bailout 20
       fi
    fi
 
-   if [ -d "$CHROOT_OUTPUT/bin" ] && [ -z "$UPDATE" ] && [ -z "$BUILD_ONLY" ] ; then
+   if false ; then
       log   "Skipping stage 'fai $FAI_ACTION' as $CHROOT_OUTPUT exists already."
       ewarn "Skipping stage 'fai $FAI_ACTION' as $CHROOT_OUTPUT exists already." ; eend 0
    else
@@ -989,6 +994,11 @@ if [ -n "$BOOTSTRAP_ONLY" ] ; then
   log   "Skipping stage 'boot' as building with bootstrap only."
   ewarn "Skipping stage 'boot' as building with bootstrap only." ; eend 0
 else
+  einfo "Installing media files from chroot build"
+  log   "Installing media files from chroot build"
+  cp --preserve=timestamp -r "$CHROOT_OUTPUT/grml-live/media/." "$BUILD_OUTPUT/"
+  eend 0
+
   # Install boot code
   mkdir -p "$BUILD_OUTPUT"/boot/"${SHORT_NAME}"
 
@@ -1584,9 +1594,10 @@ create_netbootpackage() {
   mkdir -p "$NETBOOT"
 
   local OUTPUTDIR="${NETBOOT}/build_tmp"
-  local WORKING_DIR="${OUTPUTDIR}/${OUTPUT_NAME}/tftpboot/"
 
+  local WORKING_DIR="${OUTPUTDIR}/${OUTPUT_NAME}/"
   mkdir -p "$WORKING_DIR"
+  cp --preserve=timestamp -r "$CHROOT_OUTPUT/grml-live/netboot/." "$WORKING_DIR/"
 
   cp --preserve=timestamp "${CHROOT_OUTPUT}"/boot/vmlinuz-*    "$WORKING_DIR"/vmlinuz
   cp --preserve=timestamp "${CHROOT_OUTPUT}"/boot/initrd.img-* "$WORKING_DIR"/initrd.img

commit 189876d9c43d9024507033d83dbe456763be3203

minifai: fix netboot_dir value
Fixes: d46f91b633aa8ff424ea9a7559c40d76da252155

usr/lib/grml-live/grml_live/minifai.py
@@ -721,7 +721,7 @@ def _create_dirs(chroot_dir: Path) -> BuildDirectories:
         media_dir_inside=f"/{build_dir_relative}/{media_dir_name}/",
         media_dir=media_dir,
         netboot_dir_inside=f"/{build_dir_relative}/{netboot_dir_name}/",
-        netboot_dir=media_dir,
+        netboot_dir=netboot_dir,
         sources_dir_inside=f"/{build_dir_relative}/{sources_dir_name}/",
         sources_dir=sources_dir,
     )