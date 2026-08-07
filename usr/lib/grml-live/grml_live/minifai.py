grml/grml-live

commit 6cc0d93841e8164bb4541ea5334255bd0d713797

grml-live: move mksquashfs to minifai

usr/lib/grml-live/grml_live/minifai.py
@@ -39,6 +39,7 @@ class FaiAction(StrEnum):
     SOFTUPDATE = "softupdate"
     RECONFIGURE = "reconfigure"
     REBUILD = "rebuild"
+    REBUILD_MEDIA = "rebuild_media"
 
 
 @dataclass
@@ -712,6 +713,57 @@ def copy_directory_out(
     )
 
 
+def _build_mksquashfs_options(conf_dir: Path) -> list[str]:
+    squashfs_excludes_file = conf_dir / "grml" / "squashfs-excludes"
+    options = [
+        "-noappend",
+        # use block size 1m as this gives good result with regards to time + compression
+        "-b",
+        "1m",
+        "-comp",
+        "xz",
+        # Ignore all extended attributes. This avoids:
+        # 1) leaking containerization supplied selinux attributes into the squashfs,
+        # 2) prevents unpacking errors in a later build-only step in containers not supporting xattrs.
+        "-no-xattrs",
+        # Static file exclusion list.
+        "-wildcards",
+        "-ef",
+        str(squashfs_excludes_file),
+        "-one-file-system",
+    ]
+    return options
+
+
+def mksquashfs(
+    conf_dir: Path,
+    grml_cd_dir: Path,
+    chroot_dir: Path,
+    grml_name: str,
+    unshared_service: UnsharedService,
+):
+    live_dir = grml_cd_dir / "live"
+    squashfs_dir = live_dir / grml_name
+    squashfs_file = squashfs_dir / f"{grml_name}.squashfs"
+
+    mksquashfs_binary = os.environ["MKSQUASHFS_BINARY"]
+    options = _build_mksquashfs_options(conf_dir)
+    args = [mksquashfs_binary, str(chroot_dir) + "/", squashfs_file, *options]
+
+    # We must run mksquashfs inside the userns so it sees the correct ownership info,
+    # but we want the resulting file to be owned by the user outside of the userns.
+    unshared_service.batch(
+        [
+            unshared_helper.ensure_dir(live_dir),
+            unshared_helper.ensure_dir(squashfs_dir),
+            unshared_helper.run_program(args),
+            unshared_helper.chown(squashfs_file, str(UNSHARE_UID), str(UNSHARE_GID)),
+            unshared_helper.chown(squashfs_dir, str(UNSHARE_UID), str(UNSHARE_GID)),
+            unshared_helper.chown(live_dir, str(UNSHARE_UID), str(UNSHARE_GID)),
+        ]
+    )
+
+
 def _run_tasks(
     conf_dir: Path,
     output_dir: Path,
@@ -744,6 +796,9 @@ def _run_tasks(
             grml_live_config_chroot, "\n".join(f"{k}={shlex.quote(v)}" for k, v in grml_live_config.items())
         )
     )
+
+    grml_name = os.environ["GRML_NAME"]
+    print(f"I: GRML_NAME: {grml_name!r}")
 
     do_skiptask(dynamic_state, skip_tasks)
 
@@ -783,6 +838,10 @@ def _run_tasks(
             if not should_skip_task(dynamic_state, "configure"):
                 for class_name in classes:
                     run_class_scripts("scripts", conf_dir, chroot_dir, class_name, helper_tools_paths, env)
+
+            if not should_skip_task(dynamic_state, "squashfs"):
+                # mksquashfs is the last thing that should need the userns.
+                mksquashfs(conf_dir, grml_cd_dir, chroot_dir, grml_name, unshared_service)
 
             if not should_skip_task(dynamic_state, "build"):
                 for class_name in classes:
@@ -863,6 +922,8 @@ def _main(program_name: str, argv: list[str]) -> int:
                 skiptasks = ["updatebase", "instsoft"]
             elif args.action == FaiAction.REBUILD:
                 skiptasks = ["updatebase", "instsoft", "configure"]
+            elif args.action == FaiAction.REBUILD_MEDIA:
+                skiptasks = ["updatebase", "instsoft", "configure", "squashfs"]
             else:
                 print(f"E: minifai: Unknown fai action: {args.action!r}")
                 rc = 1

grml-live
@@ -139,7 +139,6 @@ fi
 # clean exit {{{
 bailout() {
   [ -n "$CONFIGDUMP"      ]  && rm -f  "$CONFIGDUMP"
-  [ -n "$SQUASHFS_STDERR" ]  && rm -rf "$SQUASHFS_STDERR"
   [ -n "$1" ] && EXIT="$1" || EXIT="1"
   [ -n "$2" ] && eerror "$2">&2
   log "------------------------------------------------------------------------------"
@@ -414,8 +413,20 @@ if [ -n "$BUILD_ONLY" ] && [ -n "$UPDATE" ] ; then
   eerror "Only one of -b and -u can be given at a time. Remove one of them."
   bailout 1
 fi
+if [ -n "$BUILD_ONLY" ] && [ -n "$SKIP_MKSQUASHFS" ] ; then
+  eerror "Only one of -b and -q can be given at a time. Remove one of them."
+  bailout 1
+fi
 if [ -n "$BUILD_DIRTY" ] && [ -n "$UPDATE" ] ; then
   eerror "Only one of -B and -u can be given at a time. Remove one of them."
+  bailout 1
+fi
+if [ -n "$BUILD_DIRTY" ] && [ -n "$SKIP_MKSQUASHFS" ] ; then
+  eerror "Only one of -B and -q can be given at a time. Remove one of them."
+  bailout 1
+fi
+if [ -n "$UPDATE" ] && [ -n "$SKIP_MKSQUASHFS" ] ; then
+  eerror "Only one of -u and -q can be given at a time. Remove one of them."
   bailout 1
 fi
 # }}}
@@ -605,6 +616,8 @@ elif [ -n "$UPDATE" ] ; then
   FAI_ACTION=softupdate
 elif [ -n "$BUILD_DIRTY" ] ; then
   FAI_ACTION=rebuild
+elif [ -n "$SKIP_MKSQUASHFS" ] ; then
+  FAI_ACTION=rebuild_media
 else
   FAI_ACTION=dirinstall
 fi
@@ -624,6 +637,10 @@ if [ "$FAI_ACTION" = dirinstall ] && [ -r "$CHROOT_OUTPUT/etc/debian_version" ] 
 fi
 
 mkdir -p "$CHROOT_OUTPUT" || bailout 5 "Problem with creating $CHROOT_OUTPUT"
+
+# minifai needs to see these
+export GRML_NAME
+export MKSQUASHFS_BINARY
 
 log "Executed FAI command line:"
 log "${FAI_PROGRAM} ${GRML_FAI_CONFIG} ${CLASSES} ${FAI_ACTION} ${OUTPUT} ${CONFIGDUMP} ${SUITE} ${BOOTSTRAP_MIRROR}"
@@ -657,44 +674,6 @@ if [ -n "$CHROOT_INSTALL" ] ; then
      rsync -avz --inplace "$CHROOT_INSTALL"/ "$CHROOT_OUTPUT/"
      eend $?
   fi
-fi
-
-if [ -n "$SKIP_MKSQUASHFS" ] ; then
-   log   "Skipping stage 'squashfs' as requested via option -q"
-   ewarn "Skipping stage 'squashfs' as requested via option -q" ; eend 0
-else
-   mkdir -p "$BUILD_OUTPUT"/live/"${GRML_NAME}"/
-
-   # use block size 1m as this gives good result with regards to time + compression
-   SQUASHFS_OPTIONS="-b 1m -comp xz"
-
-   # Ignore all extended attributes. This avoids:
-   # 1) leaking containerization supplied selinux attributes into the squashfs,
-   # 2) prevents unpacking errors in a later build-only step in containers not supporting xattrs.
-   SQUASHFS_OPTIONS="$SQUASHFS_OPTIONS -no-xattrs"
-
-   # Static file exclusion list.
-   SQUASHFS_OPTIONS="${SQUASHFS_OPTIONS} -wildcards -ef ${GRML_FAI_CONFIG}/grml/squashfs-excludes"
-
-   # log stuff
-   SQUASHFS_STDERR="$(mktemp -t grml-live.XXXXXX)"
-   einfo "Squashfs build information: running $MKSQUASHFS_BINARY using options: $SQUASHFS_OPTIONS"
-
-   log "$MKSQUASHFS_BINARY $CHROOT_OUTPUT/ $BUILD_OUTPUT/live/${GRML_NAME}/${GRML_NAME}.squashfs -noappend $SQUASHFS_OPTIONS"
-   # shellcheck disable=SC2086 # $SQUASHFS_OPTIONS needs splitting
-   if "$MKSQUASHFS_BINARY" "$CHROOT_OUTPUT/" "$BUILD_OUTPUT"/live/"${GRML_NAME}"/"${GRML_NAME}".squashfs \
-      -noappend $SQUASHFS_OPTIONS 2>"${SQUASHFS_STDERR}" ; then
-      echo "${GRML_NAME}.squashfs" > "$BUILD_OUTPUT"/live/"${GRML_NAME}"/filesystem.module
-      log "Finished execution of stage 'squashfs' [$(date)]"
-      einfo "Finished execution of stage 'squashfs'" ; eend 0
-   else
-      log    "Error: there was a critical error executing stage 'squashfs' [$(date)]:"
-      log    "$(cat "$SQUASHFS_STDERR")"
-      eerror "Error: there was a critical error executing stage 'squashfs':"
-      cat    "${SQUASHFS_STDERR}"
-      eend 1
-      bailout
-   fi
 fi
 
 # create md5sum file:
@@ -800,15 +779,6 @@ if hasclass RELEASE && [ "$RC" = 0 ] ; then
        touch -r "${ISO_NAME}" "${ISO_NAME}.sha256"
      fi
    )
-fi
-
-if [ "$RC" = 0 ] ; then
-   log   "Finished execution of stage 'iso build' [$(date)]"
-   einfo "Finished execution of stage 'iso build'" ; eend 0
-else
-   log    "Error: there was a critical error ($RC) executing stage 'iso build' [$(date)]"
-   eerror "Error: there was a critical error executing stage 'iso build'" ; eend 1
-   bailout "$RC"
 fi
 # }}}
 

usr/lib/grml-live/grml_live/unshared_helper.py
@@ -62,6 +62,11 @@ def mkdir(path: Path | str):
 
 
 @_operation
+def ensure_dir(path: Path | str):
+    Path(path).mkdir(exist_ok=True)
+
+
+@_operation
 def ensure_empty_dir(path: Path | str):
     path = Path(path)
     if path.exists():
@@ -103,6 +108,12 @@ def have_text_in_file(path: Path | str, text: str):
 def unlink(path: Path | str):
     path = Path(path)
     path.unlink()
+
+
+@_operation
+def chown(path: Path | str, numeric_owner: str, numeric_group: str):
+    path = Path(path)
+    os.chown(str(path), int(numeric_owner), int(numeric_group), follow_symlinks=False)
 
 
 @_operation

commit f7f552d1b6f30a5814f2cfd8d9dafd57fbe801a2

minifai: write filesystem.module file
Was missed when transplanting the mksquashfs code from grml-live.

Fixes: 6cc0d93841e8164bb4541ea5334255bd0d713797

usr/lib/grml-live/grml_live/minifai.py
@@ -817,6 +817,7 @@ def mksquashfs(
     live_dir = grml_cd_dir / "live"
     squashfs_dir = live_dir / grml_name
     squashfs_file = squashfs_dir / f"{grml_name}.squashfs"
+    filesystem_module_file = squashfs_dir / "filesystem.module"
 
     mksquashfs_binary = os.environ["MKSQUASHFS_BINARY"]
     options = _build_mksquashfs_options(conf_dir)
@@ -829,6 +830,8 @@ def mksquashfs(
             unshared_helper.ensure_dir(live_dir),
             unshared_helper.ensure_dir(squashfs_dir),
             unshared_helper.run_program(args),
+            unshared_helper.write_file_text(filesystem_module_file, squashfs_file.name),
+            unshared_helper.chown(filesystem_module_file, str(UNSHARE_UID), str(UNSHARE_GID)),
             unshared_helper.chown(squashfs_file, str(UNSHARE_UID), str(UNSHARE_GID)),
             unshared_helper.chown(squashfs_dir, str(UNSHARE_UID), str(UNSHARE_GID)),
             unshared_helper.chown(live_dir, str(UNSHARE_UID), str(UNSHARE_GID)),